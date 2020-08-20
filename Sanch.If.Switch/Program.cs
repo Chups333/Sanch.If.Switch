a1ef68405204f3d95f3b5b89a78a3ada1dba10e6
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 18:34:54 +0800

    Добавьте файлы проекта.

---
+++ Sanch.If.Switch/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.If.Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+
+
+             if (Условие)
+             {
+                 Путь 1
+             }
+             else
+             {
+                 Путь 2
+             }
+             /////////////////////////////////////////////
+             bool b = true; //false
+             <,<=,>,>=,==,!=
+             if (b)
+             {
+                 Путь 1
+             }
+             else
+             {
+                 Путь 2
+             }
+             //////////////////////////////////////////////////////
+              if (3<5)-true
+             {
+                 Путь 1
+             }
+             else - false
+             {
+                 Путь 2
+             }
+
+             */
+            ////////////////////////////////////////////
+            /* int input = int.Parse(Console.ReadLine());
+             if (input > 10)
+             {
+                 Console.WriteLine("Больше");
+
+             }else
+             {
+                 Console.WriteLine("Меньше");
+
+             }
+             Console.ReadLine();*/
+            /////////////////////////////////////////////////
+            /*int input = int.Parse(Console.ReadLine());
+            if (input < 10)
+            {
+                if (input > 0) { 
+                Console.WriteLine("1");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("2");
+
+            }
+            Console.ReadLine();*/
+            /////////////////////////////////////////
+            /*int input = int.Parse(Console.ReadLine());
+            if (input < 10 && input > 0)////// && -и ||-или !-не - логика % - остаток при делении на число
+            {
+              
+                    Console.WriteLine("1");
+                
+
+
+            }
+            else
+            {
+                Console.WriteLine("2");
+
+            }
+            Console.ReadLine();*/
+            /////////////////////////////////////////
+
+            /*string s = "Привет";
+            if (s.Equals("привет", StringComparison.CurrentCultureIgnoreCase))////(s=="Привет")-не совсем правильно - способ сравнения(строк) с без учета регистра 
+            {
+
+                Console.WriteLine("1");
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("2");
+
+            }
+            Console.ReadLine(); */
+            //////////////////////////////////////////////////////////////
+            /* int input = int.Parse(Console.ReadLine());
+             if (input == 1)
+             {
+
+             }else
+                 if (input == 2)
+             {
+
+             }
+             else
+             {
+
+             }*/
+
+            ////////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////
+            //SWITCH SWITCH SWITCH SWITCH SWITCH SWITCH
+            /*switch (переменная)
+            {
+                case значение1:
+                //действия 1
+                //действия 2
+                //действия 3
+                    break;
+                case значение2:
+                //действия 1
+                //действия 2
+                    break; 
+                case значение3:
+                case значение4: !!!!!!!!!!!!!!!!!!!!!!!!!!!!Вместе!!!!!!!!!!!!!!!!!!!!
+                //действия 1
+                //действия 2
+                //действия 3
+                    break;
+
+                default:
+                    break;
+            }*/
+            ///////////////////////////////////////
+            ///////////////////////////////////////////
+            ///////////////////////////////////////////
+            // Оператор быстрой проверки
+
+            /*int input = int.Parse(Console.ReadLine());
+           var s = input == 10 ? "да" : "нет"; //////////////////////// это оно!!!!!!!!!!
+            Console.WriteLine(s);//////////////прикольно!!!!!!!!!!!!!
+            Console.ReadKey();*/
+
+           /////////////////////////////////////
+           //goto
+
+
+        }
+    }
+}
---
+++ Sanch.If.Switch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Sanch.If.Switch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sanch.If.Switch")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("375d6eca-ad86-4e9c-8ad7-b5448c198e5e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
